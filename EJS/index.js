5a046857751406b970fc3ccd04e5d18119def99d
Abhishekh Yadav <study.abhishekh@gmail.com>
2026-03-28 23:51:55 +0530

    asdfgh

--- EJS/index.js
+++ EJS/index.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+function requireToken(req, res, next) {
+    if (req.query.token === 'secret') {
+        return next();
+    }
+    res.status(401).send('Unauthorized: token is required');
+}
 app.get('/', (req, res) => {
-    res.render('home',{name:'Abhishekh'});
-})
-app.listen(3000); 
+    res.render('home', { name: 'Abhishekh' });
+});
+app.get('/protected', requireToken, (req, res) => {
+    res.send('Protected content');
+});
+app.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000');
+}); 
