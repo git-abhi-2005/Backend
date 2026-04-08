2df45ba3f515cd0540f59c7a9afa39d0a9ac2c1d
Abhishekh Yadav <study.abhishekh@gmail.com>
2026-04-08 13:16:36 +0530

    52

--- EJS/app.js
+++ EJS/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -20,9 +23,19 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', UserSchema);
 
+app.post('/user', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 mongoose.connect("mongodb://127.0.0.1:27017/mydatabase")
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Could not connect to MongoDB", err));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.listen(8000, () => {
+    console.log("Server running on port 8000");
+});
